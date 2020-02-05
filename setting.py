f13ab5ba218e36baad6d5365a859ee41b34dae34
lans <wylans@163.com>
2020-02-05 15:28:34 +0800

    添加子弹

--- setting.py
+++ setting.py
@@ -6,3 +6,9 @@ class Settings():
         self.scree_height = 720
         self.bg_color = (65, 105, 225)
         self.ship_speed_factor = 1.5
+        # 添加子弹
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullet_allow = 3
